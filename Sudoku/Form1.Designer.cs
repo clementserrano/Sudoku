f8ea3a87951f3dfad01120c303fee3b18ac7f780
Clément Serrano <clementserrano3@gmail.com>
2016-12-08 16:03:29 +0100

    commit initial

--- Sudoku/Form1.Designer.cs
+++ Sudoku/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sudoku
 {
-    partial class Form1
+    partial class VueSudoku
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // VueSudoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "VueSudoku";
+            this.Text = "Sudoku";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Sudoku/Form1.cs
+++ Sudoku/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Sudoku
 {
-    public partial class Form1 : Form
+    public partial class VueSudoku : Form
     {
-        public Form1()
+        public VueSudoku()
         {
             InitializeComponent();
         }
